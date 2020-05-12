06d9317c0087951044c039775d78f10125d690cd
Patrick <hsiang851108@gmail.com>
2020-05-12 16:18:16 +0800

    Filter added

--- src/main/java/ssl/ois/timelog/AppConfig.java
+++ src/main/java/ssl/ois/timelog/AppConfig.java
@@ -1,12 +1,19 @@
 package ssl.ois.timelog;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import ssl.ois.timelog.controller.filter.AuthFilter;
 import ssl.ois.timelog.controller.memory.MemoryLogRepository;
 import ssl.ois.timelog.controller.memory.MemoryUserRepository;
 import ssl.ois.timelog.service.log.LogRepository;
 import ssl.ois.timelog.service.user.UserRepository;
+
+import java.util.Arrays;
 
 @Configuration
 @ComponentScan
@@ -19,5 +26,50 @@ public class AppConfig {
     @Bean
     public UserRepository getUserRepository() {
         return new MemoryUserRepository();
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthFilter> loggingFilter(){
+        FilterRegistrationBean<AuthFilter> authorizationBean = new FilterRegistrationBean<>();
+        authorizationBean.setFilter(new AuthFilter());
+        authorizationBean.addUrlPatterns("/*");
+        return authorizationBean;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        //允許跨網域請求的來源
+        config.addAllowedOrigin("*");
+
+        //允許跨域攜帶cookie資訊，預設跨網域請求是不攜帶cookie資訊的。
+        config.setAllowCredentials(true);
+
+        //允許使用那些請求方式
+//        config.addAllowedMethods("GET", "PUT", "POST","DELETE");
+        config.setAllowedMethods(Arrays.asList("GET", "PUT", "POST","DELETE"));
+//        config.addAllowedMethod(HttpMethod.POST);
+
+        //允許哪些Header
+        config.addAllowedHeader("*");
+        //config.addAllowedHeader("x-firebase-auth");
+
+        //可獲取哪些Header（因為跨網域預設不能取得全部Header資訊）
+        config.addExposedHeader("/*");
+//        config.addExposedHeader("Content-Type");
+//        config.addExposedHeader( "X-Requested-With");
+//        config.addExposedHeader("accept");
+//        config.addExposedHeader("Origin");
+//        config.addExposedHeader( "Access-Control-Request-Method");
+//        config.addExposedHeader("Access-Control-Request-Headers");
+
+
+        //映射路徑
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //return一個的CorsFilter.
+        return new CorsFilter(configSource);
     }
 }
